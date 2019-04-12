20d2ecd8be04375253c1117de7fc62f29da505e3
Gilbert Takarasel <49542765+Gilberttakarasel@users.noreply.github.com>
2019-04-12 09:33:36 +0700

    Latihan
    Coba

---
+++ src/tugas1.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+public class tugas1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for(int i = 5; i>=0; i--){
+            for(int k=1; k<(i*2);k++){
+                System.out.print(k);
+            }
+            System.out.println("");
+            for(int j=6; j>i; j--){
+                System.out.print(" ");
+            }
+        }  
+    }
+}
+    
+    
+
